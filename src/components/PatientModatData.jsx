ad083b3a03755023dc9cf9d00b6bcc17841450fa
Samarth Mule <samarthmule7276@gmail.com>
2024-03-27 13:28:52 +0530

    Upadetd the PatientModatData.jsx file

--- src/components/PatientModatData.jsx
+++ src/components/PatientModatData.jsx
@@ -7,36 +7,35 @@ import {
   ModalBody,
   ModalCloseButton,
   useDisclosure,
-  Button,
-
+  UnorderedList,
+  ListItem,
+  Button, Flex
 } from "@chakra-ui/react";
 
-const PatientModatData = ({
-    isOpen,
-    onClose,
-    searchData
-  }) => {
+const PatientModatData = ({ isOpen, onClose, searchedData }) => {
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <ModalOverlay />
       <ModalContent>
-        <ModalHeader>Modal Title</ModalHeader>
+        <ModalHeader>
+        {searchedData !== null ? searchedData.patient_name : ''}
+        </ModalHeader>
         <ModalCloseButton />
         <ModalBody>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium
-          velit, beatae harum vero fugit eius facilis pariatur mollitia corporis
-          reiciendis aspernatur debitis nostrum consequuntur officiis molestias
-          quod minus, non eaque ab iste quidem. Eaque ipsam consequatur amet
-          culpa soluta illo asperiores itaque tempore animi quibusdam nostrum
-          nemo omnis, aliquam ducimus ad deleniti dignissimos iusto odit
-          architecto iure, ab officiis vitae. Quod eum nisi sit.
+          {console.log("===  PatientModetData ===", searchedData)}
         </ModalBody>
-
+        <UnorderedList fontSize={"20px"} padding={'30px'}>
+          <ListItem> Mobile No. : {searchedData !== null ? searchedData.mobile_no : ''}</ListItem>
+          <ListItem> Age : {searchedData !== null ? searchedData.agev : ''}</ListItem>
+          <ListItem> Weight : {searchedData !== null ? searchedData.weight : ''}</ListItem>
+          <ListItem> Gender : {searchedData !== null ? searchedData.gender : ''}</ListItem>
+          <ListItem> Symptoms : {searchedData !== null ? searchedData.symptoms : ''}</ListItem>
+          <ListItem> </ListItem>
+        </UnorderedList>
         <ModalFooter>
           <Button colorScheme="blue" mr={3} onClick={onClose}>
             Close
           </Button>
-          <Button variant="ghost">Secondary Action</Button>
         </ModalFooter>
       </ModalContent>
     </Modal>
